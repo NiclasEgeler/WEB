64460a2672b5da968bd934e3f0b161f1057f0db4
Pascal Hagenlocher <pascalihagen2013@gmail.com>
2023-11-20 14:10:36 +0100

    game over dialog

--- public/javascripts/minesweeper.js
+++ public/javascripts/minesweeper.js
@@ -5,12 +5,27 @@ document.querySelectorAll(".cell").forEach((element) => {
         var x = element.getAttribute("data-x");
         var y = element.getAttribute("data-y");
 
-        if(event.button == 0)
-        window.location.href = `/place/${x}/${y}`;
+        if(event.button == 0) {
+            console.log("left click")
+            window.location.href = `/place/${x}/${y}`;
+        }
+        // fetch if cell is mine when api is available. If yes show alert box with game over and play again popup
+        //$('#gameOverModal').modal('show');
         if(event.button == 2)
         window.location.href = `/flag/${x}/${y}`;
     });
     element.addEventListener('contextmenu', (event) => {
         event.preventDefault();
     });
+});
+
+document.querySelectorAll(".mine").forEach((element) => {
+    element.addEventListener("click", (event)=>  {
+        event.preventDefault();
+        $('#gameOverModal').modal('show');
+    });
+});
+
+document.getElementById("playAgainButton").addEventListener("click", function() {
+    window.location.href = '/undo';
 });
